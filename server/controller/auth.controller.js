328766e1ba8559a0c6f1e0904bce4ca550362d92
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-06-22 20:33:56 +0530

    login,logout,profile,updateprofile function created

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -80,6 +80,138 @@ const register = async (req, res, next) => {
     }
 }
 
+const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return next(new AppError("Email and password is required", 400))
+        }
+
+        const user = await User.findOne({ email }).select('+password')
+
+        if (!user) {
+            return next(new AppError("Email is not registered", 400))
+        }
+
+        const passwordCheck = await user.comparePassword(password)
+        if (!passwordCheck) {
+            return next(new AppError("Password is wrong", 400))
+        }
+
+        const token = await user.generateJWTToken()
+        res.cookie('token', token, cookieOption)
+
+        res.status(200).json({
+            success: true,
+            message: "Login successful",
+            user
+        })
+
+    } catch (e) {
+        return next(new AppError(e.message, 500))
+    }
+}
+
+const logout = async (req, res, next) => {
+    try {
+        const token = ""
+        const cookieOption = {
+            logoutAt: new Date(),
+            httpOnly: true,
+            secure: true,
+            sameSite: 'None'
+        }
+        res.cookie("token", token, cookieOption)
+        res.status(200).json({
+            success: true,
+            message: "Logout successful",
+        })
+    }
+    catch (e) {
+        return next(new AppError(e.message, 500))
+    }
+}
+
+const profile = async (req, res, next) => {
+    try {
+        const userId = req.user.id
+
+        const user = await User.findById(userId)
+
+        res.status(200).json({
+            success: true,
+            message: "User details",
+            user
+        })
+
+    }
+    catch (e) {
+        return next(new AppError(e.message, 500))
+    }
+}
+
+const updateProfile = async (req, res, next) => {
+    try {
+        const { fullName } = req.body
+        const { id } = req.user
+
+        const user = await User.findById(id)
+
+        if (!user) {
+            return next(new AppError("User does not exist", 400))
+        }
+
+        if (fullName) {
+            user.fullName = await fullName
+        }
+
+        if (req.file) {
+            // Destroying the previous avatar in cloudinary
+            await cloudinary.v2.uploader.destroy(user.avatar.publicId)
+            try {
+                // Uploading the new avatar to cloudinary
+                const result = await cloudinary.v2.uploader.upload(req.file.path, {
+                    folder: 'lms',
+                    width: 250,
+                    height: 250,
+                    gravity: 'faces',
+                    crop: 'fill',
+                })
+                // Updating user's avatar information
+                if (result) {
+                    user.avatar.publicId = result.public_id
+                    user.avatar.secure_url = result.secure_url
+
+                    // Removing the temporary file after avatar upload
+                    fs.rm(`uploads/${req.file.filename}`)
+                }
+            }
+            catch (err) {
+                // Handling errors during avatar upload
+                return next(new AppError('File can not get uploaded', 500))
+            }
+        }
+
+        // Saving the updated user document
+        await user.save()
+
+        // Sending success response to the client
+        res.status(200).json({
+            success: true,
+            message: 'User Detail updated successfully'
+        })
+    }
+
+    catch (e) {
+        return next(new AppError(e.message, 500))
+    }
+}
+
 export {
-    register
+    register,
+    login,
+    logout,
+    profile,
+    updateProfile
 }
